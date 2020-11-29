9f6e8b55b78fb25fa9376fe0312acf50f8036cb5
Denis Shakhvorostov <shakhvorostovdenis@gmail.com>
2020-11-29 12:18:15 -0800

    +

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Message
+from .forms import ContactForm
+
 
 # Create your views here.
+def contact(request):
+    error = ''
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main:index')
+        else:
+            error = 'Form not correct'
+
+
+    form = ContactForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request,'contact/contact.html', context)
